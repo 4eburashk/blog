5e76b29fae511e384c2b92c72d03450169d88347
4eburashk <csn@net4me.net>
2021-04-11 05:36:43 +0300

    change pin to PB2

--- avr/attiny13/004_blinky_with_timer_overflow/main.c
+++ avr/attiny13/004_blinky_with_timer_overflow/main.c
@@ -7,12 +7,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define	LED_PIN	PB0
+#define	LED_PIN	PB2
 
-ISR(TIM0_OVF_vect)
-{
-
-	PORTB ^= _BV(LED_PIN); // toggle LED pin
+ISR(TIM0_OVF_vect) {
+	PORTB ^= (1<<2); // change LED pin state
 }
 
 int
@@ -20,7 +18,7 @@ main(void)
 {
 
 	/* setup */
-	DDRB = 0b00000001; // set LED pin as OUTPUT
+	DDRB = 0b00000100; // set LED pin as OUTPUT
 	PORTB = 0b00000000; // set all pins to LOW
 	TCCR0B |= _BV(CS02)|_BV(CS00); // set prescaler to 1024 (CLK=1200000Hz/1024/256=4Hz, 0.25s)
 	TIMSK0 |= _BV(TOIE0); // enable Timer Overflow interrupt
