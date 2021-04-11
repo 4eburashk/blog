b753b9e1adb6e36e4a324caaba7031b58dd9da15
4eburashk <csn@net4me.net>
2021-04-11 05:10:41 +0300

    change pin to PB2

--- avr/attiny13/003_randomly_flashing_led_with_prng_based_on_lfsr/main.c
+++ avr/attiny13/003_randomly_flashing_led_with_prng_based_on_lfsr/main.c
@@ -7,32 +7,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define	LED_PIN		PB0
-#define LFSR_SEED 	(91)
-#define	DELAY		(64)
+#define LFSR_SEED 	(100)
+#define	DELAY		(500)
 
 static uint16_t
-prng_lfsr16(void)
-{
+prng_lfsr16(void) {
         static uint16_t cnt16 = LFSR_SEED;
         return (cnt16 = (cnt16 >> 1) ^ (-(cnt16 & 1) & 0xB400));
 }
 
-int
-main(void)
-{
-
+int main(void) {
 	/* setup */
-	DDRB = 0b00000001; // set LED pin as OUTPUT
-	PORTB = 0b00000000; // set all pins to LOW
-
+	DDRB = 0b00000100; // set LED pin as OUTPUT
+	PORTB = 0xFF; // set all pins to LOW
 	/* loop */
 	while (1) {
-                if (prng_lfsr16() & 1) {
-                        PORTB |= _BV(LED_PIN); // LED on
-                } else {
-                        PORTB &= ~(_BV(LED_PIN)); // LED off
+		if (prng_lfsr16() & 1) {
+		        PORTB |= (1<<2); // LED on
+		} else {
+		        PORTB &= ~(1<<2); // LED off
 		}
-                _delay_ms(DELAY);
+    _delay_ms(DELAY);
 	}
 }
